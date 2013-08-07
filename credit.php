b04e4a841fb7b6fe59a0bb92831c2eb8fe921239
tomGoodenough <tom-goodenough@hotmail.co.uk>
2013-08-07 22:18:59 +0100

    Create credit.php
    Add yourself if you contribute significantly!

---
+++ credit.php
@@ -0,0 +1,41 @@
+<html>
+<head>
+<style type="text/css">
+</style>
+<title>shared.</title>
+</head>
+<body>
+
+<?php include('inc/header.php'); ?>
+
+
+<p style="font-size:40px;">Who made shared.?</p>
+<p>Developer credits:</p>
+Tom Goodenough - creator
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include('inc/footer.php'); ?>
